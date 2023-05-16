1ce9c58c2ffd8560a4b466cf55d016ab43e90673
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2023-05-17 02:26:45 +0500

    Added Functionality of Automatically adding time and date of task at that instant

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ const input_box = document.getElementById("input-box");
 const list_container = document.getElementById("list-container");
 const add_button = document.getElementById("addButton");
 
+function getDateAndTime() {
+    const daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const today = new Date();
+    let hour = today.getHours();
+    const minute = today.getMinutes();
+    const second = today.getSeconds();
+    const prepand = hour >= 12 ? " PM" : " AM";
+
+    hour = hour % 12 || 12;
+
+    if (hour === 12 && minute === 0 && second === 0) {
+        prepand === " PM" ? (hour = "Noon") : (hour = "Midnight");
+    }
+
+    const todayDay = daylist[today.getDay()];
+    const returnData = `${todayDay}: ${hour}${prepand} : ${minute} min : ${second} sec`;
+    return returnData;
+}
+
 input_box.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.key === "Enter") {
@@ -9,13 +28,13 @@ input_box.addEventListener("keyup", function(event) {
     }
 });
 
-
 function addTask(){
     if(input_box.value === ""){
         alert("You Must Write Something!");
     }else{
         let li = document.createElement("li");
-        li.innerHTML = input_box.value;
+        let finalData = getDateAndTime();
+        li.innerHTML = input_box.value +"<br>"+finalData;
         list_container.appendChild(li);
 
         let span = document.createElement("span");
@@ -43,4 +62,11 @@ function saveData(){
 function displayData(){
     list_container.innerHTML = localStorage.getItem("data");
 }
+
+// function removeData(){
+//     const key = "data";
+//     // Remove the data from the local storage
+//     localStorage.removeItem(key);
+// }
+// removeData();
 displayData();
